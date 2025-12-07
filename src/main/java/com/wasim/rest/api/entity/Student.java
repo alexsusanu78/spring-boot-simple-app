98453adf8249be302183cb97630439cbe597398f
alexsusanu78 <alexsusanu78@gmail.com>
2025-12-07 16:00:32 +0200

    initial commit"

---
+++ src/main/java/com/wasim/rest/api/entity/Student.java
@@ -0,0 +1,21 @@
+package com.wasim.rest.api.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "student")
+public class Student {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+    private String name;
+    private String email;
+    private String address;
+}
